a333fe2666f425c6696ca4acab7096f63ee050f1
Andrew Lu <andrewgege@me.com>
2017-12-03 12:40:21 -0800

    Update spotifyauth.js

--- spotifyauth.js
+++ spotifyauth.js
@@ -2,48 +2,23 @@ function startup() {
   var code = getParameterByName('code');
   var error = getParameterByName('error');
 
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
+  var posturl = new RegExp("https://accounts.spotify.com/api/token?grant_type=authorization_code&code=" + code + "&redirect_uri=https%3A%2F%2Fdrew4n.github.io%2Fspotifyauthmid.html");
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": posturl,
+    "method": "POST",
+    "headers": {
+      "authorization": "Basic ZmNiZWNmNzJhMWZmNDI0YmFlNzhmZjJlNmM3NzllNjM6ZTZmZTdiM2M1MTJkNGFjOTk4NWE2NmRmOTI2NDEwNTM=",
+      "content-type": "application/x-www-form-urlencoded"
     }
-  };
+  }
 
-  var text = "grant_type=authorization_code&code=AUTHCODE&redirect_uri=https%3A%2F%2Fdrew4n.github.io%2Fspotifyauthmid.html";
-  var postquery = text.replace("AUTHCODE", code);
-  
-  xhttp.open("POST", "https://accounts.spotify.com/api/token", true);
-  xhttp.setRequestHeader("Authorization", "Basic ZmNiZWNmNzJhMWZmNDI0YmFlNzhmZjJlNmM3NzllNjM6ZTZmZTdiM2M1MTJkNGFjOTk4NWE2NmRmOTI2NDEwNTM=");
-  xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhttp.send(postquery);
-
-
-  //post('https://accounts.spotify.com/api/token', {grant_type: 'authorization_code', code: code, redirect_uri: 'https%3A%2F%2Fdrew4n.github.io%2Fspotifyauthmid.html', client_id: 'fcbecf72a1ff424bae78ff2e6c779e63', client_secret: 'e6fe7b3c512d4ac9985a66df92641053'})
-}
-
-function post(path, params, method) {
-    method = method || "post"; // Set method to post by default if not specified.
-
-    // The rest of this code assumes you are not using a library.
-    // It can be made less wordy if you use one.
-    var form = document.createElement("form");
-    form.setAttribute("method", method);
-    form.setAttribute("action", path);
-
-    for(var key in params) {
-        if(params.hasOwnProperty(key)) {
-            var hiddenField = document.createElement("input");
-            hiddenField.setAttribute("type", "hidden");
-            hiddenField.setAttribute("name", key);
-            hiddenField.setAttribute("value", params[key]);
-
-            form.appendChild(hiddenField);
-        }
-    }
-
-    document.body.appendChild(form);
-    form.submit();
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    document.getElementById("demo").innerHTML = response;
+  });
 }
 
 function getParameterByName(name, url) {
