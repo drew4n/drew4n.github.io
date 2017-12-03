e58066b9a7be89390240b0fa56b910687f1ebedd
Andrew Lu <andrewgege@me.com>
2017-12-02 20:12:52 -0800

    Update spotifyauth.js

--- spotifyauth.js
+++ spotifyauth.js
@@ -4,7 +4,7 @@ function startup() {
 
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+    if (this.readyState == 4) {
       document.getElementById("demo").innerHTML =
       this.responseText;
     }
