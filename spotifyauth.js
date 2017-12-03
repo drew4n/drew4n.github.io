1a2483f4bf1d35056f464249d60f3e886f14f000
Andrew Lu <andrewgege@me.com>
2017-12-02 18:10:54 -0800

    Create spotifyauth.js

---
+++ spotifyauth.js
@@ -0,0 +1,40 @@
+function startup(e) {
+  var code = getParameterByName('code');
+  var error = getParameterByName('error');
+  
+  post('https://accounts.spotify.com/api/token', {grant_type: 'authorization_code', code: code, redirect_uri: 'https%3A%2F%2Fhttpbin.org%2Fget'})
+}
+
+function post(path, params, method) {
+    method = method || "post"; // Set method to post by default if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
